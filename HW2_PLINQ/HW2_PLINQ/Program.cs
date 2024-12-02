4c5e82449de50d6adca579b8634c714138a1afa7
Warning233 <fgcgvdj@gmail.com>
2024-12-02 15:39:57 +0300

    Second HW

---
+++ HW2_PLINQ/HW2_PLINQ/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HW2_PLINQ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 1000000); // doesn't work at 1 000 000 and more.
+            var query =
+                numbers
+                    .AsParallel()
+                    .Where(IsPerfectNumber)
+                    .ToList();
+
+            foreach (var v in query)
+            {
+                Console.WriteLine(v);
+            }
+        }
+
+        private static bool IsPerfectNumber(int n)
+        {
+            if (n < 2)
+                return false;
+            
+            int sum = Enumerable.Range(1, n / 2)
+                .AsParallel() 
+                .Where(divisor => n % divisor == 0) 
+                .Sum(); 
+            
+            return sum == n;
+        }
+    }
+}
